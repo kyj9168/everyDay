420c58dbd712c002a40585ae9c49662ebb372298
youngjunkim <kyj9168@gmail.com>
2022-01-31 18:49:19 +0900

    스크롤 추가

--- src/components/views/ArticlePage/index.js
+++ src/components/views/ArticlePage/index.js
@@ -52,7 +52,16 @@ function ArticlePage({ match, location }) {
                         </div>
                         <div style={{ padding: '10px', textAlign: 'center' }}>{title}</div>
 
-                        <div style={{ padding: '10px', textAlign: 'center' }}>{ReactHtmlParser(content)}</div>
+                        <div
+                            style={{
+                                padding: '10px',
+                                textAlign: 'center',
+                                overflowY: 'auto',
+                                height: 'calc(100vh - 300px)',
+                            }}
+                        >
+                            {ReactHtmlParser(content)}
+                        </div>
                     </div>
 
                     <div className="btnDiv">
